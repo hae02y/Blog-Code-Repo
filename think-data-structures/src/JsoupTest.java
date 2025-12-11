ec9e3c666a8eb4ad8e0ffd9f56bdbfeb4b00c4a7
hae02y <godud1118@gmail.com>
2025-12-11 17:32:01 +0900

    JsoupTest 구현

---
+++ think-data-structures/src/JsoupTest.java
@@ -0,0 +1,21 @@
+import org.jsoup.Connection;
+import org.jsoup.Jsoup;
+import org.jsoup.nodes.Document;
+import org.jsoup.nodes.Element;
+import org.jsoup.select.Elements;
+
+import java.io.IOException;
+
+public class JsoupTest {
+    static void main() throws IOException {
+        String url = "https://blog.hae02y.me";
+        Connection connect = Jsoup.connect(url);
+
+        Document elements = connect.get();
+
+        Element body = elements.body();
+        Elements p = body.select("div");
+
+        System.out.println(p);
+    }
+}
---
+++ think-data-structures/src/Profiler.java
@@ -0,0 +1,34 @@
+import javafx.application.Application;
+import javafx.scene.Scene;
+import javafx.scene.control.Button;
+import javafx.scene.layout.BorderPane;
+import javafx.stage.Stage;
+
+public class Profiler extends Application {
+
+    @Override
+    public void start(Stage primaryStage) {
+        // 레이아웃 루트
+        BorderPane root = new BorderPane();
+
+        // 예시용 컴포넌트 하나
+        Button startButton = new Button("시작");
+        startButton.setOnAction(event -> {
+            System.out.println("Profiler 시작!");
+            // TODO: 여기서 실제 프로파일링 로직 호출
+        });
+
+        root.setCenter(startButton);
+
+        // Scene 생성
+        Scene scene = new Scene(root, 800, 600);
+
+        primaryStage.setTitle("Profiler");
+        primaryStage.setScene(scene);
+        primaryStage.show();
+    }
+
+    void main() {
+        launch();   // 여기서는 FXMLLoader 필요 X
+    }
+}
