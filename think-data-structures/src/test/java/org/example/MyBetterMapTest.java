0ffa34ff5f723dc25b205b9a8a83ec6504b33016
hae02y <godud1118@gmail.com>
2026-02-20 19:31:55 +0900

    MyBetterMap 구현

---
+++ think-data-structures/src/test/java/org/example/MyBetterMapTest.java
@@ -0,0 +1,20 @@
+package org.example;
+
+
+
+import org.junit.Before;
+
+public class MyBetterMapTest extends MyLinearMapTest {
+
+    /**
+     * @throws java.lang.Exception
+     */
+    @Before
+    public void setUp() throws Exception {
+        map = new MyBetterMap<String, Integer>();
+        map.put("One", 1);
+        map.put("Two", 2);
+        map.put("Three", 3);
+        map.put(null, 0);
+    }
+}
